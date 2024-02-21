eb7b1978ba0bd58e78c1cf2142d9fe803021e870
Massimo Cavallo <massimo.cavallo.62@gmail.com>
2024-02-21 09:55:17 +0100

    clean +

--- index.js
+++ index.js
@@ -1,88 +1,91 @@
-const csvReader = require("./utils/csvReader");
-const q = require("./utils/csv");
-const pjson = require('./package.json');
 
-let organizations;
-let csvtest = new q.csv(
-    csvPath = NaN
-    , csvDelimiter = NaN
-    , ignoreEmptyRows = NaN
-    , csvArray = NaN
-    , header = NaN
-    , columns = NaN
-    , where = NaN
-    , limit = NaN
-    );
+const fs = require('node:fs');
 
-let csv = new q.csv(
-    csvPath = '/Users/mcavallo/Desktop/dev/GitHub/csv-queryable/csv/organizations-100000.csv'
-    , csvDelimiter = ','
-    , ignoreEmptyRows = true
-    , csvArray = organizations
-    , header = NaN
-    , columns = NaN
-    , where = NaN
-    , limit = NaN
-    );
-
-/**
- * Just to check the success of the installation, also returns basic package information.
- * No parameters.
- */
-function itWorks() {
-    return JSON.stringify({"itWorks": "Yes, it works!"
-    , "package": pjson.name
-    , "version": pjson.version
-    });
-}
-
-function select(select, from, where, limit) {
-    const nq = new q.query(select,from,where,limit);
-    let result = nq.select();
-    return result;
-}
-
-function selectFromArray(csvArray, header, columns, where, limit) {
-    return csvReader.selectFromCsvArray(csvArray,header,columns, where, limit);
-}
-/**
- * Validates a CSV file and return true or false.
- *
- * @param {string} csvFile - The path to the CSV file.
- * @param {string} csvDelimiter - The CSV separator character, not mandatory. Default value = ','.
- * @param {boolean} ignoreEmptyRows - Non-mandatory parameter to handle the blank line at the end of the file.
- */
-function csvArrayToJsonArray(csvArray, columns){
-    let jrisultato = csvReader.csvArrayRowsToJson(csvArray.map(r => columns.map(i => r[columns.indexOf(i)])), columns);
-    return jrisultato
-}
-function csvMemorize(csvFile, csvDelimiter = ',', ignoreEmptyRows = true) {
+memorize = function(csvPath, ignoreEmptyRows) {    
 
     const start = Date.now();
-    let result = true;
 
     try
     {
-        let rowsInCsv = csvReader.csvFileLines(csvFile, csvDelimiter, ignoreEmptyRows).slice();
-        let firstLineLength = rowsInCsv[0].length;
-
-        rowsInCsv.forEach(element => {
-            if (element.length != firstLineLength) {
-                result = false;
+        const csvLines = csvPath =>
+        fs
+        .readFileSync(csvPath)
+        .toString('UTF8')
+        .split('\n')
+        .filter(function (el) {
+            if (ignoreEmptyRows) {
+                return el != '';
+            } else {
+                return el == el;
             }
+        })
+        .map(function (row) {
+            return row.match(/(".*?"|[^",]+)(?=\s*,|\s*$)/g);
         });
+        
         const end = Date.now();
         console.log("executiontime: " + `${(end - start)}ms.`);
 
-        return rowsInCsv;
+        return csvLines(csvPath);
+
     } catch (error) {
         const end = Date.now();
         console.log("executiontime: " + `${(end - start)}ms.`);
         console.log(error.message)
-        result = false;
-        return result;
     }
 }
 
+select = function(csvArray, header, columns, where, limit) {
 
-module.exports = {itWorks, csvMemorize, select, csvArrayToJsonArray, selectFromArray, csvtest}
+    const start = Date.now();
+
+    try
+    {
+        where.forEach(element => {
+            csvArray = csvArray.filter(function(e) {
+                switch(element[1])
+                {
+                    case '==':
+                    if (e[header.indexOf(element[0])] == element[2]) {
+                        return e;
+                    }
+                    break;
+                    case '!=':
+                    if (e[header.indexOf(element[0])] != element[2]) {
+                        return e;
+                    }
+                    break;
+                    case '%%':          
+                    if (e[header.indexOf(element[0])].search(element[2]) > -1) {
+                        return e;
+                    }
+                    break;
+                }          
+            });
+        });
+    
+        // map with returned colums
+        csvArray = csvArray.map(r => columns.map(i => r[header.indexOf(i)])).slice(limit[0], limit[1]);
+    
+        // JSON output
+        const csvArrayToJson = (rows) => {  
+            return rows.reduce((jsonArray, row) => {
+                const item = row.reduce((item, value, index) => {
+                return {...item, [columns[index]]: value};
+                }, {});
+                return jsonArray.concat(item);
+            }, []);
+        };
+        
+        const csvJsonArray = csvArrayToJson(csvArray);
+            
+        return JSON.stringify(csvJsonArray);
+
+    } catch (error) {
+        const end = Date.now();
+        console.log("executiontime: " + `${(end - start)}ms.`);
+        console.log(error.message)
+    }
+}
+
+module.exports = { memorize, select };
--- test/test.js
+++ test/test.js
@@ -1,19 +1,18 @@
-const q = require('csv-queryable');
+const { dirname } = require('path');
+const appDir = dirname(require.main.filename);
+const csvPath = '/Users/mcavallo/Desktop/dev/GitHub/csv-queryable/csv/organizations-100000.csv';
+const csvArrayDelimiter = ','
+const ignoreEmptyRows = true;
 const header = new Array("Index","Organization Id","Name","Website","Country","Description","Founded","Industry","Number of employees");
 const columns = new Array("Name","Website","Country","Founded");
-const where = new Array(["Country","%%","Ital"],["Founded","!=","1999"]);
+const where = new Array(["Country","%%","Italy"],["Founded","==","2011"]);
 const limit = new Array(1,2);
 
-//console.log(q.itWorks());
-
-let a = q.csvMemorize('/Users/mcavallo/Desktop/dev/GitHub/csv-queryable/csv/organizations-100000.csv');
+const csvq = require("csv-queryable");
 const start = Date.now();
-let b = q.selectFromArray(a, header, columns, where, limit);
-let c = q.csvArrayToJsonArray(b, columns);
+let a = csvq.memorize(csvPath, ignoreEmptyRows);
 const end = Date.now();
-//this.getFilteredJsonArray(this.getFilteredJsonArray(test, columns, "Country", "China"), columns, "Website", "http://sharp.com/")
-
-console.log(c);
-console.log(JSON.parse(c).length);
 console.log("executiontime: " + `${(end - start)}ms.`);
-
+let b = csvq.select(a, header, columns, where, limit, csvArrayDelimiter);
+console.log(b);
+console.log(JSON.parse(b).length);
